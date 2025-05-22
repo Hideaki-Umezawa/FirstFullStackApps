36cff86ca3d44fd3ba6d203e8df1817b0bb24284
Hideaki-Umezawa <hideaki_umezawa@mail.toyota.co.jp>
2025-05-22 13:29:50 +0900

    production production をDATABASE_URL にした

--- db/knexfile.js
+++ db/knexfile.js
@@ -18,13 +18,7 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
+    production: process.env.DATABASE_URL,
     migrations: {
       directory: "./migrations",
     },
