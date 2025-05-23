7fd54a5054b9d9873f7bbf9c3192b2db94559331
Hideaki-Umezawa <hideaki_umezawa@mail.toyota.co.jp>
2025-05-23 13:19:39 +0900

    log遷移せず

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,9 @@
 import Header from "./components/Header/Header";
-import Login from "./components/Login/Login";
 import Record from "./components/RecordPage/Record";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RecordList from "./components/RecordPage/RecordList";
-import { useContext } from "react";
+// import { useContext } from "react";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
